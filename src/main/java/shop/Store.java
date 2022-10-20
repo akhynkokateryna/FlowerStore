d99ea1c60a230cf9251659151ba143a274520495
Kateryna Akhynko <kateryna.akhynko@ucu.edu.ua>
2022-10-20 13:57:01 +0300

    added some files to git

---
+++ src/main/java/shop/Store.java
@@ -0,0 +1,33 @@
+package shop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Store {
+    private final List<FlowerBucket> flowBuckets = new ArrayList<FlowerBucket>();
+
+    public void add(FlowerBucket flowerBucket) {
+        flowBuckets.add(flowerBucket);
+    }
+
+    public boolean search(Bundle toSearch) {
+        if (toSearch instanceof FlowerBucket) {
+            for (FlowerBucket bucket : flowBuckets) {
+                if (bucket.equals(toSearch)) {
+                    System.out.println("Bucket is in store");
+                    return true;
+                }
+            }
+        } else {
+            for (FlowerBucket bucket : flowBuckets) {
+                for (FlowerPack pack: bucket.getFlowPacks()) {
+                    if (pack.getFlower().equals(toSearch)) {
+                        System.out.println("The flower is in one of the buckets");
+                        return true;
+                    }
+                }
+                }
+            }
+        return false;
+    }
+}
